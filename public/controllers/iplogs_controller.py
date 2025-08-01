f84b9973ac81579271db14ccbfe9314139f3ce78
Mess1Ass <1783806041@qq.com>
2025-08-01 17:29:49 +0800

    修改为保存前端网页主机名

--- public/controllers/iplogs_controller.py
+++ public/controllers/iplogs_controller.py
@@ -6,7 +6,7 @@ iplogs_bp = Blueprint("iplogs_bp", __name__)
 
 @iplogs_bp.route("/saveip", methods=["GET"])
 def collect_ip():
-    domain = request.host
+    domain = request.args.get("hostname")
     ip = request.headers.get("X-Forwarded-For", request.remote_addr)
 
     insert_res, insert_code = iplogs_service.insert_iplog(ip, domain)
@@ -17,7 +17,7 @@ def collect_ip():
 
 @iplogs_bp.route("/getipcnt", methods=["GET"])
 def get_visit_count():
-    domain = request.host
+    domain = request.args.get("hostname")
     cnt_res, cnt_code = iplogs_service.get_visit_count(domain)
     if cnt_code != 200:
         return jsonify(cnt_res), cnt_code
